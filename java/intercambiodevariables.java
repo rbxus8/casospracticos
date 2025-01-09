4f3067a1c755a6372592bb4d402fe4b4536c94c4
rbxus8 <115742051+rbxus8@users.noreply.github.com>
2025-01-08 23:49:58 -0500

    Add files via upload

---
+++ java/intercambiodevariables.java
@@ -0,0 +1,17 @@
+public class intercambiodevariables {
+
+    public static void main(String[] args) {
+        
+
+        /*realizar un intercambio de variables
+         * por ejemplo que v1 vale 35
+         * y v2 vale 20,realizar las acciones necesarias
+         * para que los valores se cambien 
+         * una vez se cambien el resultado q se cambie
+         * en pantalla
+        */
+
+
+
+    }
+}
